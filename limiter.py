4ed6b4ef8e1872b9fb3dbbba23e7e1885180f3fa
Mehdi Yousefi <42382118+im-api@users.noreply.github.com>
2022-11-08 02:47:26 +0330

    Update limiter.py

--- limiter.py
+++ limiter.py
@@ -9,8 +9,8 @@ import schedule;
 _db_address = '/etc/x-ui/x-ui.db'
 _max_allowed_connections = 1
 _checkCycle = 5 #seconds
-_telegrambot_token = '5758012062:AAE8BArx_BlgbY5zsrpEzrOvmUKxw3FaYcs'
-_telegram_chat_id = '763234354' # you can get this in @cid_bot bot.
+_telegrambot_token = ''
+_telegram_chat_id = '' # you can get this in @cid_bot bot.
 _sv_addr = 'TEST SV'
 def getUsers():
     conn = sqlite3.connect(_db_address)
